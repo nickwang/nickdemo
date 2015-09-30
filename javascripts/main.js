8ca1ac29a6f8864a501ce0b18b81a2356f25d00a
nickwang <lianghuwang@gmail.com>
2015-09-30 17:16:48 +0800

    Update main.js
    test modify  from nick github.com

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,1 @@
-console.log('This would be the main JS file. just modify test from nick  android studio!');
+console.log('This would be the main JS file. just modify test from nick  android studio! modify from github');
